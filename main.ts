77fc7d50e65d8451a79a1605346025f4ffffc636
Sandikxya <s_l224@student.usc.edu.au>
2025-03-18 12:37:45 +1000

    added heartbeat block in the code

--- main.ts
+++ main.ts
@@ -4,19 +4,8 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     command2 = commandparts[0]
     if (command2 == "heart") {
         basic.showIcon(IconNames.Heart)
-        for (let index = 0; index < 2; index++) {
-            basic.showIcon(IconNames.Heart)
-            basic.pause(500)
-            basic.showIcon(IconNames.SmallHeart)
-            basic.pause(500)
-        }
     } else if (command2 == "diamond") {
         basic.showIcon(IconNames.Target)
-        basic.pause(500)
-        basic.showIcon(IconNames.Diamond)
-        basic.pause(500)
-        basic.showIcon(IconNames.SmallDiamond)
-        basic.pause(500)
     } else {
         if (command2 == "square") {
             basic.showLeds(`
@@ -26,10 +15,13 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
                 # # # # #
                 # # # # #
                 `)
-            basic.showIcon(IconNames.Square)
-            basic.pause(500)
-            basic.showIcon(IconNames.SmallSquare)
-            basic.pause(500)
+        } else if (("command" as any) == ("heartbeat" as any)) {
+            for (let index = 0; index < 2; index++) {
+                basic.showIcon(IconNames.Heart)
+                basic.pause(500)
+                basic.showIcon(IconNames.SmallHeart)
+                basic.pause(500)
+            }
         } else {
             if (("command" as any) == ("sensitivity" as any)) {
                 if (commandparts.length >= 1) {
